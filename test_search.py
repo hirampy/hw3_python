290afd5ff4fe683084d1510ab48e31348bbf9bac
hirampy <155140416+hirampy@users.noreply.github.com>
2024-03-27 00:01:32 +0300

    com+opt+L

--- test_search.py
+++ test_search.py
@@ -6,14 +6,15 @@ from selene import browser, have, be
 def browser_management():
     browser.config.driver_name = 'chrome'
     browser.config.base_url = 'https://google.com'
-    browser.driver.set_window_size(1080,800)
+    browser.driver.set_window_size(1080, 800)
     browser.open('/')
+
 
 def test_valid_search():
     browser.element('[name="q"]').should(be.blank).type('qaguru').press_enter()
     browser.element('[id="search"]').should(have.text('QA.GURU'))
 
+
 def test_invalid_search():
     browser.element('[name="q"]').should(be.blank).type('123sqdfqsdf&^*%*').press_enter()
     browser.element('[id="center_col"]').should(have.text('ничего не найдено.'))
-
